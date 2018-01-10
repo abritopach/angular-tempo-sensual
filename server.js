64bef7a9164695af73886d62addd669310532d82
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-01-10 11:43:01 +0100

    Updated heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 // Run the app by serving the static files in the dist directory.
+console.log(__dirname);
 app.use(express.static(__dirname + "/dist"));
 // Start the app by listening on the default Heroku port.
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function() {
+  console.log("App listening on port 3000!");
+});
